b3c13239c4b6959a47eb60d135fe7eade932b709
John Samir <101985968+johnsamir555@users.noreply.github.com>
2023-05-29 12:20:11 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 // init Isotope
-var iso = new Isotope( '.grid', {
-  itemSelector: '.collection-list',
-  getSortData: {
-    name: '.name',
-    category: '[data-category]'
-  },
-  masonry: {
-    columnWidth: 200
-  }
-});
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item'
+}
+):
+
 // filter items on button click
 $(".filter-button-group").on("click", "button", function () {
   var filterValue = $(this).attr("data-filter");
