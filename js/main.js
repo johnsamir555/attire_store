8ecdc67c3d9fff590ce1221a28ef66773ca3ff62
johnsamir555 <jshsoft@gmail.com>
2023-05-29 17:15:38 +0300

    fix filtering

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 // init Isotope
-var $grid = $('.grid').isotope({
-  itemSelector: '.grid-item'
+var $list = $('.collection-list').isotope({
+  itemSelector: '.collection-item',
+  layoutMode: 'fitRows'
 }
-):
+);
+
 
 // filter items on button click
-$(".filter-button-group").on("click", "button", function () {
+$(".filter-button-group button").on("click", function () {
   var filterValue = $(this).attr("data-filter");
   reset();
   $(this).addClass("active-filter-btn");
-  $grid.isotope({ filter: filterValue });
+  $list.isotope({ filter: filterValue });
 });
+
 // Remove 'active-filter-btn' class from all buttons
 var filterBtns = $(".filter-button-group").find("button");
 function reset() {
